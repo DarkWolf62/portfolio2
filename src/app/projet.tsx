fa4b5c6e86c4d7f27b6e4c0ad59d944bdb2b8f62
nicolas <nicolasnourry61@gmail.com>
2024-11-01 17:48:17 +0100

    test

--- src/app/projet.tsx
+++ src/app/projet.tsx
@@ -9,7 +9,7 @@ export default function Projet() {
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
 
                 <div className="bg-gray-200 p-4 rounded-lg shadow-md">
-                    <Image src="/portfolio2/portrait.jpg" alt="Projet 1" width={400} height={300}
+                    <Image src="/portfolio2/portrait.webp" alt="Projet 1" width={400} height={300}
                            className="rounded"/>
                     <h3 className="text-xl font-semibold mt-4">Projet 1</h3>
                     <p className="text-gray-600">
